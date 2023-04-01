e5601bfcaeaac23ca3ebf5964d4b64d54d284b8b
vicbermad <vicbermad@alu.edu.gva.es>
2023-04-01 19:44:42 +0200

    Adición de métodos acelerar y decelerar.

--- src/Coche.java
+++ src/Coche.java
@@ -1,3 +1,11 @@
 public class Coche {
     public int velocidad;
+
+    public void acelerar(int aceleracion) {
+        velocidad += aceleracion;
+    }
+
+    public void decelerar(int deceleracion) {
+        velocidad -= deceleracion;
+    }
 }
--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -7,4 +7,17 @@ public class TestCoche {
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
+    @Test
+    public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
+        Coche nuevoCoche = new Coche();
+        nuevoCoche.acelerar(30);
+        Assertions.assertEquals(30, nuevoCoche.velocidad);
+    }
+    @Test
+    public void test_al_decelerar_un_coche_su_velocidad_disminuye(){
+        Coche nuevoCoche = new Coche();
+        nuevoCoche.velocidad = 50;
+        nuevoCoche.decelerar(20);
+        Assertions.assertEquals(30, nuevoCoche.velocidad);
+    }
 }
