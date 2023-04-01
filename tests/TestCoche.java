8203d8c13fed3eb07130a3807fa1758c86bc6023
vicbermad <vicbermad@alu.edu.gva.es>
2023-04-01 17:59:04 +0200

    Creación de la clase Coche, assertions y test.

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -1,8 +1,10 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void test_crear_coche(){
-        Coche nuevoCoche = new Coche;
+    public void test_al_crear_un_coche_su_velocidad_es_cero(){
+        Coche nuevoCoche = new Coche();
+        Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 }
