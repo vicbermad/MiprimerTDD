10e013a79a54bd6e8950e25d13e10461f17b4db2
vicbermad <vicbermad@alu.edu.gva.es>
2023-04-01 20:32:19 +0200

    Rama Refactorizado + nombre métodos.

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -3,25 +3,25 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void test_al_crear_un_coche_su_velocidad_es_cero(){
+    public void test_al_crear_un_coche_su_velocidad_es_cero_victor_berenguer(){
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
+    public void test_al_acelerar_un_coche_su_velocidad_aumenta_victor_berenguer(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.acelerar(30);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_decelerar_un_coche_su_velocidad_disminuye(){
+    public void test_al_decelerar_un_coche_su_velocidad_disminuye_victor_berenguer(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
         nuevoCoche.decelerar(20);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero(){
+    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero_victor_berenguer(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
         nuevoCoche.decelerar(80);
